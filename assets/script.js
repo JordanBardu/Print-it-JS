f17a1088f5e5b07a05b9ffbc7d21d044f9ac6d2f
Jordan Bardu <jordan.bardu.etu@univ-lille.fr>
2025-03-26 09:18:54 +0100

    Added bullet points

--- assets/script.js
+++ assets/script.js
@@ -20,10 +20,34 @@ const slides = [
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 
+const dotsContainer = document.querySelector(".dots"); 
+const slideLength = slides.length;
+let slideIndex = 0;
+
 arrowLeft.addEventListener('click', () => {
-	console.log('left');
+	slideIndex = slideIndex > 0 ? slideIndex-1 : slideLength-1;
+	handleBulletPoints(slideIndex);
 });
 
 arrowRight.addEventListener('click', () => {
-	console.log('right');
+	slideIndex = slideIndex < slideLength-1 ? slideIndex+1 : 0;
+	handleBulletPoints(slideIndex);
 });
+
+for (let i = 0; i < slideLength; i++) {
+	const dot = document.createElement("div");
+	dot.setAttribute("class", "dot");
+	if (i === 0) {
+		dot.classList.add("dot_selected");
+	}
+	dotsContainer.appendChild(dot);
+}
+
+function handleBulletPoints(slideIndex) {
+	const dots = document.querySelectorAll(".dot");
+	dots.forEach((dot) => {
+		dot.classList.remove("dot_selected");
+	})
+	dots[slideIndex].classList.add("dot_selected");
+}
+
