e97b69b8a4158d5752f076b46a24a42c8180681e
Jordan Bardu <jordan.bardu.etu@univ-lille.fr>
2025-03-26 07:42:10 +0100

    Added addeventlisteners on arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,14 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+
+arrowLeft.addEventListener('click', () => {
+	console.log('left');
+});
+
+arrowRight.addEventListener('click', () => {
+	console.log('right');
+});
